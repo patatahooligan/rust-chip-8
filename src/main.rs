ebe7ac066529c8c19030833fc6c0bcab7d3486dc
patatahooligan <filwn.oikonomou@gmail.com>
2020-11-08 20:34:45 +0200

    Parse rom path from command line argument

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ struct ProgramOptions {
     rom_path: String,
 }
 
-fn parse_program_options(args: std::env::Args)
-        -> Result<ProgramOptions, &'static str> {
-    return Err(&"");
+fn parse_program_options(mut args: std::env::Args)
+        -> Result<ProgramOptions, String> {
+    match args.len() {
+        0 => Err("Could not get arguments from OS. Running in non-compliant \
+                 environment?".to_owned()),
+        2 => Ok(ProgramOptions {
+            rom_path: args.nth(1).unwrap(),
+        }),
+        _ => Err("Usage: ".to_owned() + &args.nth(0).unwrap() + " path/to/rom"),
+    }
 }
 
-fn main() {
-     parse_program_options(env::args());
+fn main() -> Result<(), String> {
+     let options = parse_program_options(env::args())?;
+
+     return Ok(());
 }
